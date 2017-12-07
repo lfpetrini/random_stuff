54918ee8650ce1081025551d3b393b425db5814b
Lucas Felippe Petrini <felippepetrini@gmail.com>
2017-12-07 21:31:53 -0200

    Started the heapsort algorithm

---
+++ sorting_algorithms_c/include/heapsort.h
@@ -0,0 +1,6 @@
+#ifndef _HEAPSORT_H
+    #define _HEAPSORT_H
+#endif
+
+// Sorts an array list using the merge sort algorithm
+void heapsort(int*, int);
---
+++ sorting_algorithms_c/src/heapsort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "heapsort.h"
+
+/**
+ * heapsort(int*, int)
+ *
+ * Sorts an array list with the size passed as parameter using the heap sort algorithm.
+ *
+ * PARAMETERS:
+ *      pList -> list of integers to be sorted
+ *      pSize -> size of the list
+ *
+ */
+void heapsort(int* pList, int pSize) {
+    // nothing to be sorted if less than 2 elements
+    if(pSize < 2)
+        return;
+}
